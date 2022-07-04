2df46eb10f3857ea5e34445e47088df9440011a9
Shane <shaneiadt@gmail.com>
2022-07-04 13:10:01 +0100

    chore: tidy up scene and sprite adding

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -4,8 +4,8 @@ class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'player');
 
+    scene.add.existing(this);
     scene.physics.add.existing(this);
-    this.scene.add.existing(this);
   }
 }
 
